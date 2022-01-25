e5ccd2c75e74fc7e3a9c1cd3e60e9bee699083ba
Alex Grintsvayg <git@grin.io>
2022-01-25 17:56:24 -0500

    fixed dev

--- dev.sh
+++ dev.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-DEV=true reflex --decoration=none --start-service=true go run .
+if hash reflex 2>/dev/null; then
+  DEV=true reflex --decoration=none --start-service=true go run .
+else
+  DEV=true go run .
+fi
