ed670250e99ceabefc7a47eeac39696bd16cb38a
Alex Grintsvayg <git@grin.io>
2022-02-18 10:23:25 -0500

    pagination for cabin query

--- db/badge.go
+++ db/badge.go
@@ -8,22 +8,35 @@ import (
 )
 
 func GetCabinsForBadgeGenerator() (map[string]string, error) {
+	var cabins map[string]string
 	filterFormula := fmt.Sprintf(`AND({%s}="yes",NOT({%s}=BLANK()))`, fields.Badge, fields.Cabin)
-	response, err := defaultTable.GetRecords().
-		//FromView("view_1").
-		WithFilterFormula(filterFormula).
-		//WithSort(sortQuery1, sortQuery2).
-		ReturnFields(fields.TwitterName, fields.Cabin).
-		InStringFormat("US/Eastern", "en").
-		Do()
+	offset := ""
 
-	if err != nil {
-		return nil, errors.Wrap(err, "")
-	}
+	for {
+		response, err := defaultTable.GetRecords().
+			WithOffset(offset).
+			//FromView("view_1").
+			WithFilterFormula(filterFormula).
+			//WithSort(sortQuery1, sortQuery2).
+			ReturnFields(fields.TwitterName, fields.Cabin).
+			InStringFormat("US/Eastern", "en").
+			Do()
 
-	cabins := make(map[string]string, len(response.Records))
-	for _, r := range response.Records {
-		cabins[toStr(r.Fields[fields.TwitterName])] = toStr(r.Fields[fields.Cabin])
+		if err != nil {
+			return nil, errors.Wrap(err, "")
+		}
+
+		if cabins == nil {
+			cabins = make(map[string]string, len(response.Records))
+		}
+		for _, r := range response.Records {
+			cabins[toStr(r.Fields[fields.TwitterName])] = toStr(r.Fields[fields.Cabin])
+		}
+
+		if response.Offset == "" {
+			break
+		}
+		offset = response.Offset
 	}
 
 	return cabins, nil
